a46ada5de16512d273bd332e8d26327de9892a47
tasiavavav <vavevairina@gmail.com>
2025-06-02 21:39:09 +0300

    4_task.cpp

--- 4_task.cpp
+++ 4_task.cpp
@@ -1,71 +1,94 @@
-#include <iostream> 
-#include <cmath> 
-#include <iomanip> 
-#include <limits> 
-using namespace std; 
-/** 
- * @brief - Функция для вычисления y 
- * @return - возвращает значение y 
- */ 
-double getY(const double x); 
-/** 
- * @brief считывает значение с клавиатуры с проверкой ввода 
- * @return введенное значение 
- */ 
-double getValue(); 
-/** 
- * @brief проверяет существование шага 
- * @param step - шаг 
- */ 
-void checkStep(const double step); 
-/** 
- *@brief - Точка входа в программу 
- *@return - возвращает 0, если программа выполнена корректно 
- */ 
-int main() {
-    setlocale(LC_ALL, "RU");
-    cout << "Введите начало интервала (xstart): ";
-    double xstart = getValue();
-    cout << "Введите конец интервала (xend): ";
-    double xend = getValue();
-    checkInterval(xstart, xend);  // Проверка, что 0 не входит в интервал
+main.cpp
 
-    cout << "Введите шаг (step): ";
-    double step = getValue();
-    checkStep(step);
+#include <iostream>
+#include <cmath>
+#include <limits>
+using namespace std;
 
-    cout << fixed << setprecision(9);
-    cout << "Табулирование функции y = cos(2/x) - 2*sin(1/x) + 1/x" << endl;
-    cout << "x\t\ty(x)" << endl;
-    cout << "---------------------" << endl;
+/* */
+@brief Crumbloom значения c кладиатуры с проберкой ввода
+@return боябращает значение, если оно правильное, иначе завершает программу */
 
-    for (double x = xstart; x <= xend + numeric_limits<double>::epsilon(); x += step) {
-        double y = getY(x);
-        if (!isnan(y)) {  // Пропускаем NaN (если x слишком близок к 0)
-            cout << x << "\t\t" << y << endl;
+double getValue();
+
+/* */
+@brief функции для вычисления значения функции y = cos(2/x) - 2*sin(1/x) + 1/x
+@return cos(2/x) - 2*sin(1/x) + 1/x
+
+double calculate_expression(const double x);
+
+/* */
+@brief функция для проберки, что шаг положительный
+@return положительный шаг
+
+double getPositiveStep();
+
+/* */
+@brief функции для проберки, что значение x входит в область определения функции (000)
+@param x значение x
+@return true, если x входит в 000, иначе false
+
+bool is00F(double x);
+
+/* */
+@brief Точка входа в программу
+@return 0
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    cout << "Введите начальное значение x: ";
+    double startX = getValue();
+    cout << "Введите конечное значение x: ";
+    double endX = getValue();
+    double step = getPositiveStep();
+    cout << "x | y" << endl;
+    cout << "---" << endl;
+
+    for (double x = startX; x <= endX + numeric_limits<double>::epsilon(); x += step)
+    {
+        if (is00F(x))
+        {
+            double y = calculate_expression(x);
+            cout << x << " | " << y << endl;
+        }
+        else
+        {
         }
     }
-    return 0;
-} 
-double getValue() 
-{ 
-    double value = 0; 
-    cin >> value; 
-    if (cin.fail()) 
-    { 
-        cout << "Incorrect value" << endl; 
-        abort(); 
-    } 
-    return value; 
-} 
-void checkStep(const double step) 
-{ 
-    if (step <= 0) 
-    { 
-        cout << "Error value" << endl; 
-        abort(); 
-    } 
-} 
-double getY(const double x) { 
-    return  cos(2.0 / x) - 2.0 * sin(1.0 / x) + 1.0 / x; 
+}
+return 0;
+
+double getValue()
+{
+    double value = 0;
+    cin >> value;
+    if (cin.fail())
+    {
+        cout << "Некорректное значение" << endl;
+        abort();
+    }
+    return value;
+}
+
+double calculate_expression(const double x) {
+    return cos(2.0 / x) - 2.0 * sin(1.0 / x) + 1.0 / x;
+}
+
+double getPositiveStep()
+{
+    double step = 0;
+    step = getValue();
+    if (step <= 0)
+    {
+        cout << "Ошибка. Шаг должен быть положительным. Повторите ввод." << endl;
+        abort();
+    }
+    return step;
+}
+
+bool is00F(double x)
+{
+    return fabs(x) > numeric_limits<double>::epsilon();
 }
