e00dbfb1d3fe0d96bfd6732196f5f9f40b669a66
tasiavavav <vavevairina@gmail.com>
2025-06-06 13:40:27 +0300

    7_task.cpp

--- 7_task.cpp
+++ 7_task.cpp
@@ -1,120 +1,168 @@
-#include <iostream>
-#include <algorithm>
-#include <iomanip>
+// Задание 1: Инвертировать максимальные элементы в строках
+    invertMaxInRows(workingArray, m, n);
+    cout << "После инверсии максимальных элементов в строках:" << endl;
+    printArray(workingArray, m, n);
 
-using namespace std;
+    // Задание 2: Вставить нулевые столбцы после столбцов с максимальным элементом
+    size_t newColumns = getNumberColumns(workingArray, m, n);
+    int** newArray = createArray(m, n + newColumns);
+    insertZeroColumns(newArray, m, n + newColumns, n, workingArray);
+    cout << "После вставки нулевых столбцов:" << endl;
+    printArray(newArray, m, n + newColumns);
 
-int main() {
-    // Ввод размеров матрицы
-    int rows, cols;
-    cout << "Введите количество строк: ";
-    cin >> rows;
-    cout << "Введите количество столбцов: ";
-    cin >> cols;
-
-    // Создание и ввод матрицы
-    int** matrix = new int*[rows];
-    cout << "\nВведите элементы матрицы:\n";
-    for (int i = 0; i < rows; i++) {
-        matrix[i] = new int[cols];
-        for (int j = 0; j < cols; j++) {
-            cout << "Элемент [" << i << "][" << j << "]: ";
-            cin >> matrix[i][j];
-        }
-    }
-
-    // Вывод исходной матрицы
-    cout << "\nИсходная матрица:\n";
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            cout << setw(5) << matrix[i][j];
-        }
-        cout << endl;
-    }
-
-    // 1. Находим максимальные элементы в каждой строке
-    int* row_max = new int[rows];
-    for (int i = 0; i < rows; i++) {
-        row_max[i] = *max_element(matrix[i], matrix[i] + cols);
-    }
-
-    // 2. Заменяем максимальные элементы на противоположные
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            if (matrix[i][j] == row_max[i]) {
-                matrix[i][j] = -row_max[i];
-                break;
-            }
-        }
-    }
-
-    cout << "\nПосле замены максимумов:\n";
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            cout << setw(5) << matrix[i][j];
-        }
-        cout << endl;
-    }
-
-    // 3. Определяем столбцы, содержащие максимальные элементы (до замены)
-    bool* has_max = new bool[cols]();
-    for (int j = 0; j < cols; j++) {
-        for (int i = 0; i < rows; i++) {
-            if (matrix[i][j] == -row_max[i]) { // Ищем измененные максимумы
-                has_max[j] = true;
-                break;
-            }
-        }
-    }
-
-    // 4. Подсчитываем новые столбцы
-    int new_cols = cols;
-    for (int j = 0; j < cols; j++) {
-        if (has_max[j]) new_cols++;
-    }
-
-    // 5. Создаем новую матрицу с нулевыми столбцами
-    int** new_matrix = new int*[rows];
-    for (int i = 0; i < rows; i++) {
-        new_matrix[i] = new int[new_cols];
-    }
-
-    // 6. Заполняем новую матрицу
-    int new_j = 0;
-    for (int j = 0; j < cols; j++) {
-        // Копируем текущий столбец
-        for (int i = 0; i < rows; i++) {
-            new_matrix[i][new_j] = matrix[i][j];
-        }
-        new_j++;
-
-        // Вставляем нулевой столбец после столбца с максимумом
-        if (has_max[j]) {
-            for (int i = 0; i < rows; i++) {
-                new_matrix[i][new_j] = 0;
-            }
-            new_j++;
-        }
-    }
-
-    // Вывод результата
-    cout << "\nРезультат с нулевыми столбцами:\n";
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < new_cols; j++) {
-            cout << setw(5) << new_matrix[i][j];
-        }
-        cout << endl;
-    }
-
-    // Освобождение памяти
-    for (int i = 0; i < rows; i++) {
-        delete[] matrix[i];
-        delete[] new_matrix[i];
-    }
-    delete[] matrix;
-    delete[] new_matrix;
-    delete[] row_max;
-    delete[] has_max;
+    // Освобождаем память
+    deleteArray(originalArray, m, n);
+    deleteArray(workingArray, m, n);
+    deleteArray(newArray, m, n + newColumns);
 
     return 0;
+}
+
+int getValue()
+{
+    int value = 0;
+    if (!(cin >> value)) {
+        cerr << "Ошибка ввода! Программа завершена." << endl;
+        abort();
+    }
+    return value;
+}
+
+size_t getSize()
+{
+    int n = getValue();
+    checkN(n);
+    return static_cast<size_t>(n);
+}
+
+void checkN(const int n)
+{
+    if (n <= 0) {
+        cerr << "Ошибка: Размер должен быть положительным!" << endl;
+        abort();
+    }
+}
+
+int** createArray(const size_t rows, const size_t cols)
+{
+    int** array = new int*[rows];
+    for (size_t i = 0; i < rows; i++) {
+        array[i] = new int[cols];
+    }
+    return array;
+}
+
+void printArray(int** array, const size_t rows, const size_t cols)
+{
+    for (size_t i = 0; i < rows; i++) {
+        for (size_t j = 0; j < cols; j++) {
+            cout << setw(6) << array[i][j];
+        }
+        cout << endl;
+    }
+}
+
+void fillArray(int** array, const size_t rows, const size_t cols)
+{
+    for (size_t i = 0; i < rows; i++) {
+        for (size_t j = 0; j < cols; j++) {
+            cout << "Введите элемент [" << i << "][" << j << "]: ";
+            array[i][j] = getValue();
+        }
+    }
+}
+
+size_t getMaxIndexInRow(int* row, const size_t n)
+{
+    size_t maxIndex = 0;
+    for (size_t i = 1; i < n; i++) {
+        if (row[i] > row[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+    return maxIndex;
+}
+
+void invertMaxInRows(int** array, const size_t rows, const size_t cols)
+{
+    for (size_t i = 0; i < rows; i++) {
+        size_t maxIndex = getMaxIndexInRow(array[i], cols);
+        array[i][maxIndex] = -array[i][maxIndex];
+    }
+}
+
+int findMaxElement(int** array, const size_t rows, const size_t cols)
+{
+    int maxElement = array[0][0];
+    for (size_t i = 0; i < rows; i++) {
+        for (size_t j = 0; j < cols; j++) {
+            if (array[i][j] > maxElement) {
+                maxElement = array[i][j];
+            }
+        }
+    }
+    return maxElement;
+}
+
+bool hasMaxElement(int** array, const size_t rows, size_t col, int maxElement)
+{
+    for (size_t i = 0; i < rows; i++) {
+        if (array[i][col] == maxElement) {
+            return true;
+        }
+    }
+    return false;
+}
+
+size_t getNumberColumns(int** array, const size_t rows, const size_t cols)
+{
+    int maxElement = findMaxElement(array, rows, cols);
+    size_t count = 0;
+
+    for (size_t j = 0; j < cols; j++) {
+        if (hasMaxElement(array, rows, j, maxElement)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+void insertZeroColumns(int newArray, const size_t rows, const size_t newCols, const size_t cols, int originalArray)
+{
+    int maxElement = findMaxElement(originalArray, rows, cols);
+    size_t newCol = 0;
+
+    for (size_t j = 0; j < cols; j++) {
+        // Копируем оригинальный столбец
+        for (size_t i = 0; i < rows; i++) {
+            newArray[i][newCol] = originalArray[i][j];
+        }
+        newCol++;
+
+        // Добавляем нулевой столбец если нужно
+        if (hasMaxElement(originalArray, rows, j, maxElement)) {
+            for(size_t i = 0; i < rows; i++) {
+                newArray[i][newCol] = 0;
+            }
+            newCol++;
+        }
+    }
+}
+
+void deleteArray(int** array, const size_t rows, const size_t cols)
+{
+    for(size_t i = 0; i < rows; i++) {
+        delete[] array[i];
+    }
+    delete[] array;
+}
+int copyArray(int array, const size_t rows, const size_t cols)
+{
+    int** newArray = createArray(rows, cols);
+    for(size_t i = 0; i < rows; i++) {
+        for(size_t j = 0; j < cols; j++) {
+            newArray[i][j] = array[i][j];
+        }
+    }
+    return newArray;
 }
