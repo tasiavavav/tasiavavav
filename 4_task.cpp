bf503a447b82b1fe9867fcb0c89bdc3d60923f74
sonik_222 <vavevairina@gmail.com>
2025-06-10 12:34:58 +0300

    4_task.cpp

--- 4_task.cpp
+++ 4_task.cpp
@@ -1,99 +1,99 @@
 #include <iostream>
 #include <cmath>
 #include <limits>
+
 using namespace std;
 
 /**
- * @brief Получает значение с клавиатуры с проверкой ввода
- * @return Возвращает значение, если оно корректное, иначе завершает программу
+ * @brief Считывает значения с клавиатуры с проверкой ввода
+ * @return Возвращает значение, если оно правильное, иначе завершает программу
  */
 double getValue();
 
 /**
- * @brief Вычисляет значение функции y = cos(2/x) - 2*sin(1/x) + 1/x
- * @param x Входное значение
- * @return Результат вычисления функции
+ * @brief Функция для вычисления значения функции y = cos(2/x) - 2*sin(1/x) + 1/x
+ * @return cos(2/x) - 2*sin(1/x) + 1/x
  */
 double calculate_expression(const double x);
 
 /**
- * @brief Проверяет, что шаг положительный
- * @return Положительный шаг
+ * @brief Функция для проверки, что шаг положительный
+ * @return положительный шаг
  */
 double getPositiveStep();
 
 /**
- * @brief Проверяет, что значение x входит в область определения функции (x ≠ 0)
- * @param x Проверяемое значение
- * @return true, если x ≠ 0, иначе false
+ * @brief Функция для проверки, что значение x входит в область определения функции (ООФ)
+ * @param x значение x
+ * @return true, если x входит в ООФ, иначе false
  */
-bool isInDomain(double x);
+bool isOOF(double x);
 
+/**
+ * @brief Точка входа в программу
+ * @return 0
+ */
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
     cout << "Введите начальное значение x: ";
     double startX = getValue();
-    
+
     cout << "Введите конечное значение x: ";
     double endX = getValue();
-    
-    cout << "Введите шаг: ";
+
     double step = getPositiveStep();
-    
+
     cout << "x | y" << endl;
-    cout << "--------" << endl;
+    cout << "---" << endl;
 
     for (double x = startX; x <= endX + numeric_limits<double>::epsilon(); x += step)
     {
-        if (isInDomain(x))
+        if (isOOF(x))
         {
             double y = calculate_expression(x);
             cout << x << " | " << y << endl;
         }
         else
         {
-            cout << x << " | Функция не определена (x=0)" << endl;
+            cout << x << " | Не входит в область определения функции (x не может быть 0)" << endl;
         }
     }
-    
+
     return 0;
 }
 
 double getValue()
 {
-    double value;
+    double value = 0;
     cin >> value;
-    
     if (cin.fail())
     {
-        cerr << "Некорректное значение. Программа будет завершена." << endl;
-        exit(EXIT_FAILURE);
+        cout << "Некорректное значение" << endl;
+        abort();
     }
-    
     return value;
 }
 
-double calculate_expression(const double x) 
+double calculate_expression(const double x)
 {
     return cos(2.0 / x) - 2.0 * sin(1.0 / x) + 1.0 / x;
 }
 
 double getPositiveStep()
 {
-    double step = getValue();
-    
+    double step = 0;
+    step = getValue();
     if (step <= 0)
     {
-        cerr << "Ошибка: шаг должен быть положительным. Программа будет завершена." << endl;
-        exit(EXIT_FAILURE);
+        cout << "Ошибка. Шаг должен быть положительным. Повторите ввод." << endl;
+        abort();
     }
-    
     return step;
 }
 
-bool isInDomain(double x)
+bool isOOF(double x)
 {
     return fabs(x) > numeric_limits<double>::epsilon();
 }
